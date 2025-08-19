6223753420910916dcf0e68fb1b18b11074081ee
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-08-20 02:10:28 +0300

    Update BookingService.java

--- src/main/java/com/university/booking/service/BookingService.java
+++ src/main/java/com/university/booking/service/BookingService.java
@@ -1,4 +1,15 @@
 package com.university.booking.service;
 
+import com.university.booking.dto.request.BookingRequest;
+import com.university.booking.dto.response.BookingResponse;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface BookingService {
+    public BookingResponse createBooking(BookingRequest request) ;
+    public List<BookingResponse> getAllBookings();
+    public BookingResponse getBookingById(Long id);
+    public BookingResponse cancelBooking(Long bookingId, Long userId, String reason);
 }
