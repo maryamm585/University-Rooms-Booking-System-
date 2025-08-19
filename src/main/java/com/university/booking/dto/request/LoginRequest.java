30b73f58c1e1985026a1961ce45ad6d7c6595b8d
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-08-20 02:03:34 +0300

    Update LoginRequest.java

--- src/main/java/com/university/booking/dto/request/LoginRequest.java
+++ src/main/java/com/university/booking/dto/request/LoginRequest.java
@@ -1,4 +1,12 @@
 package com.university.booking.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @NotBlank(message = "missed")
+    private String username ,password;
+
 }
