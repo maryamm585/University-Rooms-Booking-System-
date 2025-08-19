c4809da5779d73df02377cfb7eb14dc8dd9d4041
maryamhossam <112872236+maryamm585@users.noreply.github.com>
2025-08-20 00:00:07 +0300

    Update BuildingRepository.java

--- src/main/java/com/university/booking/repository/BuildingRepository.java
+++ src/main/java/com/university/booking/repository/BuildingRepository.java
@@ -1,4 +1,12 @@
 package com.university.booking.repository;
 
-public interface BuildingRepository {
+import com.university.booking.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+    List<Building> findByActiveTrue();
 }
