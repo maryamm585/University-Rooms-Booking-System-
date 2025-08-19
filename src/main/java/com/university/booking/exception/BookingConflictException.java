8729dc23f6ce091a323042458a9476aa7c7365f3
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-08-20 02:05:17 +0300

    Update BookingConflictException.java

--- src/main/java/com/university/booking/exception/BookingConflictException.java
+++ src/main/java/com/university/booking/exception/BookingConflictException.java
@@ -1,4 +1,7 @@
 package com.university.booking.exception;
 
-public class BookingConflictException {
+public class BookingConflictException extends RuntimeException{
+    public BookingConflictException(String message) {
+        super(message);
+    }
 }
